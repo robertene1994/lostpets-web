76912a837ccc4b7a3d54a85aff1511cc105eb309
Robert <robertene1994@gmail.com>
2020-09-21 22:55:13 +0300

    CI/CD new fix

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://lostpets-back-end.herokuapp.com',
     messagingUrl: 'wss://lostpets-back-end.herokuapp.com/lostpets-ws/websocket',
-    googleMapsApiKey: 'xaxa',
+    googleMapsApiKey: 'GOOGLE_MAPS_API_KEY',
 };
