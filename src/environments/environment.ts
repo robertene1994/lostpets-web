030e84c7db3ddc6850b224ba8f68e879cf9855fa
Robert <robertene1994@gmail.com>
2020-09-21 16:40:20 +0300

    CI/CD V2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
-import { GOOGLE_MAPS_API_KEY } from './config';
+// import { GOOGLE_MAPS_API_KEY } from './config';
 
 export const environment = {
     production: false,
     apiUrl: 'http://172.16.12.54:8080',
     messagingUrl: 'ws://172.16.12.54:8080/lostpets-ws/websocket',
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY || GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
 };
