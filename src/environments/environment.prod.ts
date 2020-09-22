dbd2a35afcb1d169122c30e5edefab20ba190c88
Robert <robertene1994@gmail.com>
2020-09-22 19:43:23 +0300

    Fixed deployment on Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ec2-3-236-13-135.compute-1.amazonaws.com:8080/LostPets',
-    messagingUrl: 'ws://ec2-3-236-13-135.compute-1.amazonaws.com:8080/LostPets/lostpets-ws/websocket',
+    apiUrl: 'https://lostpets-back-end.herokuapp.com',
+    messagingUrl: 'wss://lostpets-back-end.herokuapp.com/lostpets-ws/websocket',
     googleMapsApiKey: 'GOOGLE_MAPS_API_KEY',
 };
