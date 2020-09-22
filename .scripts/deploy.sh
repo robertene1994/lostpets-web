8b8d84dab6af26ab20e6d0b6b5c5b752231836e9
Robert <robertene1994@gmail.com>
2020-09-22 17:16:34 +0300

    -release-

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+echo "machine git.heroku.com" > ~/.netrc
+echo "login $HEROKU_EMAIL" > ~/.netrc
+echo "password $HEROKU_API_KEY" > ~/.netrc
 cd build
 ls -l
 git init
 # git config --global user.name "$HEROKU_USERNAME"
 # git config --global user.email "$HEROKU_EMAIL"
-git config --global user.name "robertene1994"
-git config --global user.email "robertene1994@gmail.com"
 git add .
 git commit -m "Release"
 git push --force https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git master
